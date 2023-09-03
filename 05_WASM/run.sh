f35206339ebc2c8b43c0f04d7431e3495d7ceb62
hsfzxjy <hsfzxjy@gmail.com>
2023-09-03 19:38:57 +0800

    Fix build script

--- 05_WASM/run.sh
+++ 05_WASM/run.sh
@@ -2,10 +2,10 @@
 
 source "$(dirname $0)/../util.sh"
 
-if !builtin command -v wasm-pack; then
+if ! builtin command -v wasm-pack; then
     cargo install wasm-pack || exit 1
 fi
-if [ ! -z "$(rustup toolchain list | grep nightly)" ]; then
+if [ -z "$(rustup toolchain list | grep nightly)" ]; then
     rustup install nightly || exit 1
 else
     echo "Rust Nightly installed."
